463afc49c6fb7d8d8925d2c4d5d55eeab16ce6bf
Rukevwe Ukwa <rukevweukwa@gmail.com>
2025-09-07 21:43:12 +0100

    Update maincontent info

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ function MainContent() {
     <main style={{ padding: '10px', backgroundColor: 'lightgray' }}>
       <h2 style={{ color: 'darkslateblue' }}>Explore Cities</h2>
       <p>
-        Here are some of the most beautiful cities you should visit around the world.
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
