fb6e53ad62d9fe4fd65bd0e5d1d62babd21837e7
Rukevwe Ukwa <rukevweukwa@gmail.com>
2025-09-17 14:07:26 +0100

    Add github-user-search project with initial setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+import SearchBar from  "./Components/SearchBar";
+
+function App()  {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <SearchBar />
+    </div>
+  );
+}
+export default App;
---
+++ github-user-search/src/Components/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SearchBar () {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search GitHub users..." />
+      <button type="submit">Search</button>
+    </div>
+  )
+}
+
+export default SearchBar
