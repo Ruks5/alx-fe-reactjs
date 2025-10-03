6903f8294af4235ae18cc1bd1f73d6f95d82f9cf
Rukevwe Ukwa <rukevweukwa@gmail.com>
2025-10-03 20:09:07 +0100

    Set up HomePage component and updated App.jsx to render it

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Tailwind is working! 🎉
-      </h1>
+    <div className="bg-gray-100 min-h-screen">
+      <HomePage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
