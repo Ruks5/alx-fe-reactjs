ba418eb35b75dffa659e49f38e40a70f8c4611fb
Rukevwe Ukwa <rukevweukwa@gmail.com>
2025-10-10 19:37:18 +0100

    fix: rename formikForm.js to formikForm.jsx to support JSX syntax

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import FormikForm from "./components/FormikForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
     <div>
       <h1>React Form Handling Project</h1>
-      <FormikForm />
+      <formikForm />
     </div>
   );
 }
