d6c9435a09bd44a80cbf6126f1a1eeeeb46dcb05
Rukevwe Ukwa <rukevweukwa@gmail.com>
2025-10-10 19:23:02 +0100

    refactor: integrate Formik and Yup for advanced form handling and validation

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
     <div>
       <h1>React Form Handling Project</h1>
-      <RegistrationForm />
+      <FormikForm />
     </div>
   );
 }
