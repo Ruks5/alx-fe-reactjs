c811c908f8e7c7b9e2a72a4a0f7c8fd54715634e
Rukevwe Ukwa <rukevweukwa@gmail.com>
2025-10-12 05:44:51 +0100

    Fix TodoList component and test import issue

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText("Add a new todo");
+    const addButton = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "Test new todo" } });
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("Test new todo")).toBeInTheDocument();
+  });
+
+  test("toggles todo completion", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+
+    expect(todo).not.toHaveStyle("text-decoration: line-through");
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Build a Todo App");
+    const deleteButton = todo.nextSibling;
+
+    fireEvent.click(deleteButton);
+    expect(screen.queryByText("Build a Todo App")).not.toBeInTheDocument();
+  });
+});
